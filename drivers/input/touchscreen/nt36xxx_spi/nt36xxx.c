ab0c28303fcca2bb21636492d2603deb4c509e97
Erfan Abdi <erfangplus@gmail.com>
2020-03-07 03:09:00 +0330

    input: nt36xxx_spi: Implement double_tap node
    [dereference23: use lct_nvt_tp_gesture_callback]
    
    Change-Id: I1ff68f35ed5b0c21f993ef339ffc3df111c60e74

--- drivers/input/touchscreen/nt36xxx_spi/nt36xxx.c
+++ drivers/input/touchscreen/nt36xxx_spi/nt36xxx.c
@@ -229,6 +229,33 @@ static struct tp_common_ops double_tap_ops = {
 };
 #endif
 
+#ifdef CONFIG_TOUCHSCREEN_COMMON
+static ssize_t double_tap_show(struct kobject *kobj,
+                               struct kobj_attribute *attr, char *buf)
+{
+    return sprintf(buf, "%d\n", ts->is_gesture_mode);
+}
+
+static ssize_t double_tap_store(struct kobject *kobj,
+                                struct kobj_attribute *attr, const char *buf,
+                                size_t count)
+{
+    int rc, val;
+
+    rc = kstrtoint(buf, 10, &val);
+    if (rc)
+    return -EINVAL;
+
+    lct_nvt_tp_gesture_callback(!!val);
+    return count;
+}
+
+static struct tp_common_ops double_tap_ops = {
+    .show = double_tap_show,
+    .store = double_tap_store
+};
+#endif
+
 /*******************************************************
 Description:
 	Novatek touchscreen irq enable/disable function.
