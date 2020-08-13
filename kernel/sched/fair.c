f2774be6453404288c8e22a908fccc81c5e2fe4e
Biao Long <blong@codeaurora.org>
2020-08-13 17:35:28 +0800

    sched:/fair: check cpu_allowed mask when find target
    Current find best target cpu is not check cpu_allowed mask, so
    it is possible to place the task on wrong cpu, add cpu mask
    check when find best target.
    
    Change-Id: I8075657fa67704ab8d9d94d542a7983bcf8d034f
    Signed-off-by: Biao Long <blong@codeaurora.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -7756,6 +7756,9 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 			long spare_cap;
 			int idle_idx = INT_MAX;
 
+			if (!cpumask_test_cpu(i, &p->cpus_mask))
+				continue;
+
 			trace_sched_cpu_util(i);
 
 			if (!cpu_online(i) || cpu_isolated(i))
