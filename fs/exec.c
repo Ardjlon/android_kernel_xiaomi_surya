c5042e9deba3f5f2118e478783074e4ce31f703e
John Galt <johngaltfirstrun@gmail.com>
2021-07-14 14:43:07 -0400

    fs/exec: update hwc + sflinger affining

--- fs/exec.c
+++ fs/exec.c
@@ -1853,13 +1853,13 @@ static int do_execveat_common(int fd, struct filename *filename,
 		else if (unlikely(!strncmp(filename->name,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
-			current->flags |= PF_PERF_CRITICAL;
+			current->pc_flags |= PC_PERF_AFFINE;;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
 		else if (unlikely(!strncmp(filename->name,
 					   SFLINGER_BIN_PREFIX,
 					   strlen(SFLINGER_BIN_PREFIX)))) {
-			current->flags |= PF_PERF_CRITICAL;
+			current->pc_flags |= PC_PERF_AFFINE;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		}
 	}
