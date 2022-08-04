e7ccc9b2c0fdbc27c00f124303084c0c7933f7c5
Cyber Knight <cyberknight755@gmail.com>
2022-08-04 16:59:31 +0000

    drm/atomic: Boost CPU & DDR bus according to set kernel profile
    - Boosting is done only when KProfiles is set to mode 2 or 3.
    - Otherwise boosting is skipped.
    - This patch is adapted to the latest implementation of KProfiles.
    
    [@pri0818]
    -Adapt to our usage
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -31,6 +31,8 @@
 #include <drm/drm_mode.h>
 #include <drm/drm_print.h>
 #include <linux/pm_qos.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
 #include <linux/sync_file.h>
 #include <linux/devfreq_boost.h>
 
@@ -2206,6 +2208,8 @@ static void complete_crtc_signaling(struct drm_device *dev,
 	kfree(fence_state);
 }
 
+extern int kp_active_mode(void);
+
 static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 				   struct drm_file *file_priv)
 {
@@ -2251,9 +2255,20 @@ static int __drm_mode_atomic_ioctl(struct drm_device *dev, void *data,
 
 	if (!(arg->flags & DRM_MODE_ATOMIC_TEST_ONLY) &&
 			df_boost_within_input(3250)) {
-		devfreq_boost_kick(DEVFREQ_MSM_CPUBW);
-		devfreq_boost_kick(DEVFREQ_MSM_LLCCBW);
-	}
+	  /*
+	   * Dont boost CPU & DDR if battery saver profile is enabled
+	   * and boost CPU & DDR if balanced profile is enabled
+	   */
+	  if (kp_active_mode() == 3 || kp_active_mode() == 0) {
+	    cpu_input_boost_kick_max(25);
+	    devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 75);
+	    devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 75);
+	  } else if (kp_active_mode() == 2) {
+	    cpu_input_boost_kick_max(15);
+	    devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 20);
+	    devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 20);
+      }
+}
 
 	drm_modeset_acquire_init(&ctx, 0);
 
