abd42f7d17b9be54c31b46d641407ebbce7f6c5e
Cyber Knight <cyberknight755@gmail.com>
2022-09-21 18:30:59 +0000

    kernel: Boost DDR bus and CPU freq according to set kernel profile
    - Boosting is done only when KProfiles is set to mode 2 or 3.
    - Otherwise boosting is skipped.
    - This patch is adapted to the latest implementation of KProfiles.
    
    [@pri0818]
    - Adapt to our usage
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    Signed-off-by: pri0818 <priyanshusinghal0818@gmail.com>

--- kernel/fork.c
+++ kernel/fork.c
@@ -97,6 +97,7 @@
 #include <linux/simple_lmk.h>
 #include <linux/cpu_input_boost.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -2243,6 +2244,7 @@ struct task_struct *fork_idle(int cpu)
 	return task;
 }
 
+extern int kp_active_mode(void);
 /*
  *  Ok, this is the main fork-routine.
  *
@@ -2260,12 +2262,22 @@ long _do_fork(unsigned long clone_flags,
 	int trace = 0;
 	long nr;
 
-	/* Boost CPU to the max for 500 ms when userspace launches an app */
+	/* Boost DDR bus to the max for 50 ms when userspace launches an app */
 	if (task_is_zygote(current)) {
-		cpu_input_boost_kick_max(500);
-		devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 500, true);
-		devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 500, true);
-	}
+	  /*
+	   * Dont boost CPU & DDR if battery saver profile is enabled
+	   * and boost CPU & DDR if balanced profile is enabled
+	   */
+	  if (kp_active_mode() == 3 || kp_active_mode() == 0) {
+	    cpu_input_boost_kick_max(50);
+	    devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 75, true);
+	    devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 75, true);
+	  } else if (kp_active_mode() == 2) {
+	    cpu_input_boost_kick_max(10);
+	    devfreq_boost_kick_max(DEVFREQ_MSM_LLCCBW, 15, true);
+	    devfreq_boost_kick_max(DEVFREQ_MSM_CPUBW, 15, true);
+      }
+}
 
 	/*
 	 * Determine whether and which event to report to ptracer.  When
