fcc74c9e2f3eb240718c85057e275b33a8e661b3
John Galt <johngaltfirstrun@gmail.com>
2023-01-10 09:00:03 -0500

    schedutil: extend kprofile battery horizon functionality

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -25,9 +25,10 @@
 static unsigned int default_efficient_freq_lp[] = {0};
 static u64 default_up_delay_lp[] = {0};
 
-static unsigned int default_efficient_freq_hp[] = {1612800};
-static u64 default_up_delay_hp[] = {70 * NSEC_PER_MSEC};
-static u64 default_up_delay_hp_batt[] = {1000 * NSEC_PER_MSEC};
+static unsigned int default_efficient_freq_hp[] = {1401600};
+static unsigned int default_efficient_freq_hp_batt[] = {1286400};
+static u64 default_up_delay_hp[] = {50 * NSEC_PER_MSEC};
+static u64 default_up_delay_hp_batt[] = {150 * NSEC_PER_MSEC};
 
 struct sugov_tunables {
 	struct gov_attr_set attr_set;
@@ -692,9 +693,15 @@ static ssize_t efficient_freq_store(struct gov_attr_set *attr_set,
 	    tunables->efficient_freq = new_efficient_freq;
 	    tunables->nefficient_freq = new_num;
 	    tunables->current_step = 0;
-	    if (old != default_efficient_freq_lp
-	     && old != default_efficient_freq_hp)
-	        kfree(old);
+	    if (kp_active_mode() == 1) {
+	    	if (old != default_efficient_freq_lp
+	     	&& old != default_efficient_freq_hp_batt)
+	        	kfree(old);
+	    } else {
+	    	if (old != default_efficient_freq_lp
+	     	&& old != default_efficient_freq_hp)
+	        	kfree(old);
+	    }
 	}
 
 	return count;
@@ -943,12 +950,14 @@ static int sugov_init(struct cpufreq_policy *policy)
 		tunables->up_delay = default_up_delay_lp;
 		tunables->nup_delay = ARRAY_SIZE(default_up_delay_lp);
 	} else if (cpumask_test_cpu(sg_policy->policy->cpu, cpu_perf_mask)) {
-		tunables->efficient_freq = default_efficient_freq_hp;
-    		tunables->nefficient_freq = ARRAY_SIZE(default_efficient_freq_hp);
 		if (kp_active_mode() == 1) {
+			tunables->efficient_freq = default_efficient_freq_hp_batt;
+			tunables->nefficient_freq = ARRAY_SIZE(default_efficient_freq_hp_batt);
 			tunables->up_delay = default_up_delay_hp_batt;
 			tunables->nup_delay = ARRAY_SIZE(default_up_delay_hp_batt);
 		} else {
+			tunables->efficient_freq = default_efficient_freq_hp;
+			tunables->nefficient_freq = ARRAY_SIZE(default_efficient_freq_hp);
 			tunables->up_delay = default_up_delay_hp;
 			tunables->nup_delay = ARRAY_SIZE(default_up_delay_hp);
 		}
