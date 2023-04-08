af5a273b3a5bd9a37e767f7e673f725fe7148db5
Ardjlon <carlosardany@gmail.com>
2023-04-08 23:28:39 +0530

    Revert "fs/exec: remove surfaceflinger force affine"
    This reverts commit dccc29a94f8cb1527963feb60b94b6b0686581e4.

--- fs/exec.c
+++ fs/exec.c
@@ -80,6 +80,7 @@ static DEFINE_RWLOCK(binfmt_lock);
 
 #define HWCOMPOSER_BIN_PREFIX "/vendor/bin/hw/android.hardware.graphics.composer"
 #define QTIHW_BIN_PREFIX "/vendor/bin/hw/vendor.qti.hardware"
+#define SFLINGER_BIN_PREFIX "/system/bin/surfaceflinger"
 #define ZYGOTE32_BIN "/system/bin/app_process32"
 #define ZYGOTE64_BIN "/system/bin/app_process64"
 static struct signal_struct *zygote32_sig;
@@ -1857,6 +1858,11 @@ static int do_execveat_common(int fd, struct filename *filename,
 					   HWCOMPOSER_BIN_PREFIX,
 					   strlen(HWCOMPOSER_BIN_PREFIX)))) {
 			current->pc_flags |= PC_PERF_AFFINE;;
+			set_cpus_allowed_ptr(current, cpu_perf_mask);
+		} else if (unlikely(!strncmp(filename->name,
+					   SFLINGER_BIN_PREFIX,
+					   strlen(SFLINGER_BIN_PREFIX)))) {
+			current->pc_flags |= PC_PERF_AFFINE;
 			set_cpus_allowed_ptr(current, cpu_perf_mask);
 		} else if (unlikely(!strncmp(filename->name,
 					   QTIHW_BIN_PREFIX,
